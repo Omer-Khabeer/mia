1151a9f2d164e24e31bd7bd678336ee120d3eee6
Omer Khaber <omerkhaber@gmail.com>
2024-12-20 09:18:41 +0100

    data2

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -1,13 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { siteData } from "@/data/siteData";
-
-import useSWR from "swr";
 import Image from "next/image";
-
 import { AudioPlayer } from "react-audio-play";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
@@ -15,12 +10,8 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-// fetcher
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
-const AlbumSlider = () => {
+const AlbumSlider = ({ albums }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const albums = siteData.albums;
 
   if (!albums || albums.length === 0) {
     return <div className="text-center py-4">No albums available</div>;
@@ -47,65 +38,56 @@ const AlbumSlider = () => {
         }}
         className="album-slider"
       >
-        {albums.map((album) => {
-          return (
-            <SwiperSlide key={album.id} className="mb-12">
-              <div className="w-full bg-secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 xl:p-12 gap-x-12">
-                {/* img */}
-                <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative cursor-pointer rounded-[10px] overflow-hidden">
-                  <Image
-                    src={album.img}
-                    fill
-                    priority
-                    alt=""
-                    className="object-contain"
-                  />
-                </div>
-                {/* track container */}
-                <div className="flex flex-1 w-full h-[500px]">
-                  {/* tracks */}
-                  <div className="flex-1 flex flex-col xl:px-12">
-                    {album.tracks?.map((track, index) => {
-                      return (
-                        <div
-                          className="flex flex-1 w-full h-[500px]"
-                          key={index}
-                        >
-                          {/* track name */}
-                          <div className="flex flex-1 items-center gap-x-2 capitalize font-semibold xl:font-extrabold">
-                            <div className="text-accent text-sm xl:text-lg">
-                              0{index + 1}.
-                            </div>
-                            <div className="text-sm xl:text-base">
-                              {track.name}
-                            </div>
-                          </div>
-                          {/* player */}
-                          <div>
-                            <AudioPlayer
-                              style={{
-                                background: "transparent",
-                                boxShadow: "none",
-                                width: "100%",
-                              }}
-                              src={track.src}
-                              loop
-                              preload="none"
-                              color="#fff"
-                              volume={40}
-                              volumePlacement="bottom"
-                              className="album-player"
-                            />
-                          </div>
+        {albums.map((album) => (
+          <SwiperSlide key={album.id} className="mb-12">
+            <div className="w-full bg-secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 xl:p-12 gap-x-12">
+              {/* img */}
+              <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative cursor-pointer rounded-[10px] overflow-hidden">
+                <Image
+                  src={album.img}
+                  fill
+                  priority
+                  alt={`Album cover for ${album.name}`}
+                  className="object-contain"
+                />
+              </div>
+              {/* track container */}
+              <div className="flex flex-1 w-full h-[500px]">
+                {/* tracks */}
+                <div className="flex-1 flex flex-col xl:px-12">
+                  {album.tracks?.map((track, index) => (
+                    <div className="flex flex-1 w-full h-[500px]" key={index}>
+                      {/* track name */}
+                      <div className="flex flex-1 items-center gap-x-2 capitalize font-semibold xl:font-extrabold">
+                        <div className="text-accent text-sm xl:text-lg">
+                          0{index + 1}.
                         </div>
-                      );
-                    })}
-                  </div>
+                        <div className="text-sm xl:text-base">{track.name}</div>
+                      </div>
+                      {/* player */}
+                      <div>
+                        <AudioPlayer
+                          style={{
+                            background: "transparent",
+                            boxShadow: "none",
+                            width: "100%",
+                          }}
+                          src={track.src}
+                          loop
+                          preload="none"
+                          color="#fff"
+                          volume={40}
+                          volumePlacement="bottom"
+                          className="album-player"
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </SwiperSlide>
-          );
-        })}
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
       {/* thumb slider */}
       <Swiper
@@ -139,25 +121,23 @@ const AlbumSlider = () => {
         watchSlidesProgress={true}
         className="thumb-slider"
       >
-        {albums?.map((thumb, index) => {
-          return (
-            <SwiperSlide
-              key={index}
-              className="relative group overflow-hidden border-2 border-transparent w-[254px] rounded-[10px]"
-            >
-              {/* img */}
-              <div className="relative w-[195px] h-[195px] sm:w-[360px] sm:h-[360px] md:w-[240px] md:max-h-[240px] cursor-pointer">
-                <Image
-                  src={thumb.img}
-                  fill
-                  priority
-                  alt=""
-                  className="object-contain group-hover:scale-105 transition-all duration-300"
-                />
-              </div>
-            </SwiperSlide>
-          );
-        })}
+        {albums?.map((thumb, index) => (
+          <SwiperSlide
+            key={index}
+            className="relative group overflow-hidden border-2 border-transparent w-[254px] rounded-[10px]"
+          >
+            {/* img */}
+            <div className="relative w-[195px] h-[195px] sm:w-[360px] sm:h-[360px] md:w-[240px] md:max-h-[240px] cursor-pointer">
+              <Image
+                src={thumb.img}
+                fill
+                priority
+                alt={`Thumbnail for ${thumb.name}`}
+                className="object-contain group-hover:scale-105 transition-all duration-300"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
