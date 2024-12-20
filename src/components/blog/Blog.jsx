50fdd1f679df0862dff4e6d0534f6e19bccfe72b
Omer Khaber <omerkhaber@gmail.com>
2024-12-20 09:08:15 +0100

    data

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,17 +1,14 @@
-import SectionHeader from '../SectionHeader';
-import PostList from './PostList';
+import SectionHeader from "../SectionHeader";
+import PostList from "./PostList";
+import { siteData } from "@/data/siteData";
 
-const getPosts = async () => {
-  const res = await fetch('http://localhost:4000/posts');
-  return res.json();
-};
+const Blog = () => {
+  const { posts } = siteData;
 
-const Blog = async () => {
-  const posts = await getPosts();
   return (
-    <section className='section' id='blog'>
-      <div className='container mx-auto'>
-        <SectionHeader pretitle='Our Blog' title='Latest News' />
+    <section className="section" id="blog">
+      <div className="container mx-auto">
+        <SectionHeader pretitle="Our Blog" title="Latest News" />
         {/* post list */}
         <PostList posts={posts} />
       </div>
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,18 +1,14 @@
-import EventBox from './EventBox';
-import SectionHeader from '../SectionHeader';
+import EventBox from "./EventBox";
+import SectionHeader from "../SectionHeader";
+import { siteData } from "@/data/siteData";
 
-const getEvents = async () => {
-  const res = await fetch('http://localhost:4000/events');
-  return res.json();
-};
-
-const Events = async () => {
-  const events = await getEvents();
+const Events = () => {
+  const { events } = siteData;
 
   return (
-    <section className='section' id='tours'>
-      <div className='container mx-auto'>
-        <SectionHeader pretitle='World Tour' title='Upcoming Events' />
+    <section className="section" id="tours">
+      <div className="container mx-auto">
+        <SectionHeader pretitle="World Tour" title="Upcoming Events" />
         {/* event box */}
         <EventBox events={events} />
       </div>
