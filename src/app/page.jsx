532dda980f65597d8262ebfc45ffda6bcbb16994
Omer Khaber <omerkhaber@gmail.com>
2024-12-20 09:14:15 +0100

    data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,22 @@
-import Hero from '@/components/Hero';
-import Player from '@/components/Player';
-import Albums from '@/components/albums/Albums';
-import Events from '@/components/events/Events';
-import Blog from '@/components/blog/Blog';
-import Newsletter from '@/components/Newsletter';
+import Hero from "@/components/Hero";
+import Albums from "@/components/albums/Albums";
+import Events from "@/components/events/Events";
+import Player from "@/components/Player";
+import Blog from "@/components/blog/Blog";
+import Newsletter from "@/components/Newsletter";
+import { siteData } from "@/data/siteData";
 
 export default function Home() {
+  const { events, albums, posts } = siteData;
+
   return (
     <main>
       <Hero />
       <Player />
-      <Events />
-      <Albums />
-      <Blog />
+      <Events events={events} />
+      <Albums albums={albums} />
+      <Blog posts={posts} />
       <Newsletter />
-      {/* <div className='h-[4000px]'></div> */}
     </main>
   );
 }
